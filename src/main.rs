9b743bc9bfccae7ee42e27848b0eb5b606b08673
Cassio Fiuza <cassio.silva@syonet.com>
2024-03-06 23:03:42 -0300

    chore: config do pool e migrations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::{postgres::PgPoolOptions, FromRow, PgPool };
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+struct Config {
+    #[clap(long, env)]
+    db_url: String,
+}
+
+struct AppState {
+    pool: PgPool
+}
+
+#[tokio::main]
+async fn main() {
+    dotenv::dotenv().ok();
+
+    let config: Config = Config::parse();
+
+    let pool = PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&config.db_url)
+        .await
+        .expect("Não foi possivel conectar ao banco de dados!");
+
+    sqlx::migrate!()
+        .run(&pool)
+        .await
+        .expect("Não foi possivel executar as migrations");
+
 }
